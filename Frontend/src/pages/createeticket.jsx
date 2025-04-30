57510e046512b4f156b9466bf1fdc241cec9f522
sarmitha123 <>
2025-04-30 17:58:53 +0530

    create notice page add

---
+++ Frontend/src/pages/createeticket.jsx
@@ -0,0 +1,18 @@
+import React from 'react';
+import NavVillager from '../components/NavVillager';
+
+import CreateTicket from '../components/ticket/create-ticket.component';
+
+const TicketVill = () => {
+
+    return (
+        <div>
+            <NavVillager/>
+            <CreateTicket/>
+
+        </div>
+
+);
+};
+
+export default TicketVill;
