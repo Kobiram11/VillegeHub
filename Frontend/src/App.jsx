b911e8cc6af7318d8edc78b65c26b1218cbdf0c8
jaindu-sasan <>
2025-04-30 15:13:55 +0530

    app.jsx is update and pro route are added

--- Frontend/src/App.jsx
+++ Frontend/src/App.jsx
@@ -3,7 +3,8 @@ import { BrowserRouter as Router, Routes, Route } from "react-router-dom";
 import './App.css'
 import ElectionVil from './pages/electionVill.jsx'
 import ElectionGrama from './pages/electionGrama.jsx';
-
+import ProceduresGrama from './pages/ProceduresGrama.jsx';
+import ProceduresVil from './pages/ProceduresVil.jsx';
 
 function App() {
   const [count, setCount] = useState(0)
@@ -15,6 +16,8 @@ function App() {
       <Route path ="/voter-rights-request" element={<ElectionVil/>} />
       <Route path= "/election" element={<ElectionGrama/>} />
 
+      <Route path= "/procedures" element={<ProceduresGrama/>} />
+      <Route path= "/villagerprocedures" element={<ProceduresVil/>} />
 
 
 
