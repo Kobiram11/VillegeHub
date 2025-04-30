584d0b4c844b3e575810f9dfd9eb0821416cb923
sarmitha123 <>
2025-04-30 17:44:28 +0530

    app.jsx upadated

--- Frontend/src/App.jsx
+++ Frontend/src/App.jsx
@@ -7,6 +7,8 @@ import ProceduresGrama from './pages/ProceduresGrama.jsx';
 import ProceduresVil from './pages/ProceduresVil.jsx';
 import HouseholdPage from './pages/householdPage.jsx';
 import Filedvisit from './pages/Filedvisit.jsx';
+import NoticeResident from './pages/NoticeResident.jsx';
+import NoticeGramaNiladhari from './pages/NoticeGramaNiladhari.jsx';
 
 function App() {
   const [count, setCount] = useState(0)
@@ -24,6 +26,8 @@ function App() {
       <Route path="/household-data" element={<HouseholdPage />} />
       <Route path="/field-visits" element={<Filedvisit />} />
 
+      <Route path= "/notices" element= {<NoticeResident/>} />
+      <Route path= "/gramaNotices" element= {<NoticeGramaNiladhari/>} />
 
 
 
