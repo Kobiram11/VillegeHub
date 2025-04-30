c7e4ed38c8043f5946998902e50a0da23fb7e764
Kobiram11 <>
2025-04-30 16:23:13 +0530

    filed visit page added

---
+++ Frontend/src/pages/Filedvisit.jsx
@@ -0,0 +1,16 @@
+import React from 'react';
+import FrontFvisit from '../components/fieldvisit/frontFvisit.js';
+import NavBar from '../components/Navbar';
+
+const Filedvisit = () => {
+    return (
+        <div className="form-container">
+          <NavBar/>
+        <FrontFvisit/>
+      </div>
+    );
+
+
+};
+
+export default Filedvisit;
